042ab7911a2f74d933f5a32cc06fe60f06da3faa
Karan Dhillon <me@kdhillon.com>
2021-10-28 09:22:09 -0400

    [feature] Add solution to 278: First Bad Version problem

---
+++ solutions/firstBadVersion.kt
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+def isBadVersion(version: Int): Boolean = {} */
+
+class Solution: VersionControl() {
+    override fun firstBadVersion(n: Int) : Int {
+        var left = 1
+        var right = n
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            
+            if (isBadVersion(mid)) right = mid
+            else left = mid + 1        
+        }
+        
+        return left
+    }
+}
